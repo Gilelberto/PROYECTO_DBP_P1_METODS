35d5f726b19004979b20d6f582906ee44285c1dd
Gil C <a348503@uach.mx>
2023-03-12 14:45:51 -0600

    Se genera la estructura básica del programa

--- met_info.sh
+++ met_info.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+opt="$1"
+
+#caso donde no se reciba un parámetro válido
+if [ $opt != "-a" ] && [ $opt != "-t" ]; then       #como es una cadena usamos != en lugar de -ne
+ echo "Error: Parámetro desconocido"
+fi
+
+#caso -a metodologías ágiles
+if [ $opt == "-a" ]; then       
+ echo "PROCEDER AL MENÚ DE AGILES"
+fi
+
+#caso -t metodologías tradicionales
+if [ $opt == "-t" ]; then       
+ echo "PROCEDER AL MENÚ DE TRADICIONALES"
+fi
