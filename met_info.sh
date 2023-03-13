e7e64cb92108cfd0439c327e3d2fd67272b83d0e
Gil C <a348503@uach.mx>
2023-03-12 18:43:48 -0600

    Se añadé lógica basada en funciones y se crean los archivos de información

--- met_info.sh
+++ met_info.sh
@@ -2,7 +2,50 @@
 opt="$1"
 opt2=0
 section=""
-met_id=0   #esta se usará en conjunto a opt2 para una función que maneje los archivos de la sección
+route="./"
+
+addInfo() {
+    echo "add"
+}
+search() {
+    echo "search"
+}
+deleteInfo() {
+    echo "delete"
+}
+readInfo() {
+    cat $route
+}
+
+submenu(){
+    aux=0
+    echo "Usted esta en la sección $section. Seleccione la opción que desea utilizar.
+
+            1.- Agregar información
+            2.- Buscar
+            3.- Eliminar información
+            4.- Leer base de información."
+    read aux
+    case $aux in
+        1)
+            addInfo
+        ;;
+        2)
+            search
+        ;;
+
+        3)
+            deleteInfo
+        ;;
+        4)
+            readInfo
+        ;;
+        *)
+            echo "Opción no válida"
+        ;;
+    esac
+    
+}
 
 #caso donde no se reciba un parámetro válido
 if [ $opt != "-a" ] && [ $opt != "-t" ]; then       #como es una cadena usamos != en lugar de -ne
@@ -11,7 +54,8 @@ if [ $opt != "-a" ] && [ $opt != "-t" ]; then       #como es una cadena usamos !
 fi
 
 #caso -a metodologías ágiles
-if [ $opt == "-a" ]; then      
+if [ $opt == "-a" ]; then
+ route+="agile/"      
  echo "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:
             1.- SCRUM
             2.- X.P.
@@ -20,20 +64,24 @@ if [ $opt == "-a" ]; then
  read opt2
  case $opt2 in
     1)
-        section+="Metodologías ágiles, SCRUM" 
-        echo "Se encuentra en la sección: $section"
+        section+="Metodologías ágiles, SCRUM"
+        route+="scrum.inf" 
+        submenu
         ;;
     2)
         section+="Metodologías ágiles, X.P."
-        echo "Se encuentra en la sección: $section"
+        route+="xp.inf"
+        submenu
         ;;
     3)
         section+="Metodologías ágiles, Kanban"
-        echo "Se encuentra en la sección: $section"
+        route+="kanban.inf"
+        submenu
         ;;
     4)
         section+="Metodologías ágiles, Crystal"
-        echo "Se encuentra en la sección: $section"
+        route+="crystal.inf"
+        submenu
         ;;
     *)
         echo "Opción no válida"
@@ -44,7 +92,8 @@ if [ $opt == "-a" ]; then
 fi
 
 #caso -t metodologías tradicionales
-if [ $opt == "-t" ]; then      
+if [ $opt == "-t" ]; then
+ route+="tradi/"      
  echo "Bienvenido a la guía rápida de metodologías tradicionales, para continuar seleccione un tema:
             1.- Cascada
             2.-Espiral
@@ -52,16 +101,19 @@ if [ $opt == "-t" ]; then
  read opt2
  case $opt2 in
     1)
-        section+="Metodologías tradicionales, Cascada" 
-        echo "Se encuentra en la sección: $section"
+        section+="Metodologías tradicionales, Cascada"
+        route+="cascada.inf" 
+        submenu
         ;;
     2)
         section+="Metodologías tradicionales, Espiral" 
-        echo "Se encuentra en la sección: $section"
+        route+="espiral.inf"
+        submenu
         ;;
     3)
         section+="Metodologías tradicionales, Modelo V" 
-        echo "Se encuentra en la sección: $section"
+        route+="modeloV.inf"
+        submenu
         ;;
     *)
         echo "Opción no válida"
@@ -70,3 +122,4 @@ if [ $opt == "-t" ]; then
  esac
  
 fi
+
