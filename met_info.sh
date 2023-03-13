02cc15eddc473597c3d63be412136204239b69f8
Mau Ramirez <a348705@uach.mx>
2023-03-12 21:28:15 -0700

    Se cambia continue por goBack, se agrega a todas las opciones y se añade opcion de regresar al menu inicial

--- met_info.sh
+++ met_info.sh
@@ -30,28 +30,31 @@ submenu(){
             2.- Buscar
             3.- Eliminar información
             4.- Leer base de información.
-            5.- Salir"
+            5.- Volver al menu inicial
+            6.- Salir"
     read aux
     case $aux in
         1)
             addInfo
+            goBack
         ;;
         2)
             search
+            goBack
         ;;
 
         3)
             deleteInfo
+            goBack
         ;;
         4)
             readInfo
-            echo " "
-            echo "Ingresa 1 para regresar al menú"
-            echo "Cualquier otra para finalizar"
-            read par
-            continue $par
+            goBack
         ;;
         5)
+            menu
+        ;;
+        6)
             exit 0
         ;;
         *)
@@ -61,9 +64,12 @@ submenu(){
     
 }
 
-continue(){
-    op=$1
-    if [ $op -eq 1 ]; then
+goBack(){
+    echo " "
+    echo "Ingresa 1 para regresar al menú anterior" 
+    echo "Cualquier otra para finalizar"
+    read par
+    if [ $par -eq 1 ]; then
         submenu
     else
         echo "Fin del programa."
@@ -72,79 +78,84 @@ continue(){
 
 }
 
-#caso donde no se reciba un parámetro válido
-if [ $opt != "-a" ] && [ $opt != "-t" ]; then       #como es una cadena usamos != en lugar de -ne
- echo "Error: Parámetro desconocido"
- exit 1
-fi
+menu(){
+    #caso donde no se reciba un parámetro válido
+    if [ $opt != "-a" ] && [ $opt != "-t" ]; then       #como es una cadena usamos != en lugar de -ne
+    echo "Error: Parámetro desconocido"
+    exit 1
+    fi
 
-#caso -a metodologías ágiles
-if [ $opt == "-a" ]; then
- route+="agile/"      
- echo "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:
+    opt2=0
+    section=""
+    route="./"
+    if [ $opt == "-a" ]; then
+    route+="agile/"  
+    echo "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:
             1.- SCRUM
             2.- X.P.
             3.- Kanban
             4.- Crystal"
- read opt2
- case $opt2 in
-    1)
-        section+="Metodologías ágiles, SCRUM"
-        route+="scrum.inf" 
-        submenu
-        ;;
-    2)
-        section+="Metodologías ágiles, X.P."
-        route+="xp.inf"
-        submenu
-        ;;
-    3)
-        section+="Metodologías ágiles, Kanban"
-        route+="kanban.inf"
-        submenu
-        ;;
-    4)
-        section+="Metodologías ágiles, Crystal"
-        route+="crystal.inf"
-        submenu
-        ;;
-    *)
-        echo "Opción no válida"
-        exit 1
-        ;;
- esac
+    read opt2
+    case $opt2 in
+        1)
+            section+="Metodologías ágiles, SCRUM"
+            route+="scrum.inf" 
+            submenu
+            ;;
+        2)
+            section+="Metodologías ágiles, X.P."
+            route+="xp.inf"
+            submenu
+            ;;
+        3)
+            section+="Metodologías ágiles, Kanban"
+            route+="kanban.inf"
+            submenu
+            ;;
+        4)
+            section+="Metodologías ágiles, Crystal"
+            route+="crystal.inf"
+            submenu
+            ;;
+        *)
+            echo "Opción no válida"
+            exit 1
+            ;;
+    esac
 
-fi
+    fi
 
-#caso -t metodologías tradicionales
-if [ $opt == "-t" ]; then
- route+="tradi/"      
- echo "Bienvenido a la guía rápida de metodologías tradicionales, para continuar seleccione un tema:
-            1.- Cascada
-            2.-Espiral
-            3.-Modelo V"
- read opt2
- case $opt2 in
-    1)
-        section+="Metodologías tradicionales, Cascada"
-        route+="cascada.inf" 
-        submenu
-        ;;
-    2)
-        section+="Metodologías tradicionales, Espiral" 
-        route+="espiral.inf"
-        submenu
-        ;;
-    3)
-        section+="Metodologías tradicionales, Modelo V" 
-        route+="modeloV.inf"
-        submenu
-        ;;
-    *)
-        echo "Opción no válida"
-        exit 1
-        ;;
- esac
- 
-fi
+    #caso -t metodologías tradicionales
+    if [ $opt == "-t" ]; then
+    route+="tradi/"      
+    echo "Bienvenido a la guía rápida de metodologías tradicionales, para continuar seleccione un tema:
+                1.- Cascada
+                2.-Espiral
+                3.-Modelo V"
+    read opt2
+    case $opt2 in
+        1)
+            section+="Metodologías tradicionales, Cascada"
+            route+="cascada.inf" 
+            submenu
+            ;;
+        2)
+            section+="Metodologías tradicionales, Espiral" 
+            route+="espiral.inf"
+            submenu
+            ;;
+        3)
+            section+="Metodologías tradicionales, Modelo V" 
+            route+="modeloV.inf"
+            submenu
+            ;;
+        *)
+            echo "Opción no válida"
+            exit 1
+            ;;
+    esac
 
+    fi
+}
+
+menu
